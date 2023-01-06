96115d825a0371f7623243fb9e7ed9d54015858e
Qingqing <qingqing.dai94@gmail.com>
2023-01-06 18:27:47 +0100

    add edit form

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,5 @@
-import React, {useState} from 'react'
-import { Puppy } from '../types/types';
+import React, {useState, useRef} from 'react'
+import { Puppy, CreatePuppy } from '../types/types';
 
 const API_LINK = 'http://localhost:8080/api/puppies';
 interface IPuppyComponentProps {
@@ -7,7 +7,14 @@ interface IPuppyComponentProps {
 }
 
 const Card = ({puppy} : IPuppyComponentProps) => {
-  const [showDetails, setShowDetails] = useState<boolean>(false);
+  const [showDetails, setShowDetails] = useState(false);
+  const [showEditForm, setShowEditForm] = useState(false);
+
+  const breedRef = useRef<HTMLInputElement>(null);
+  const nameRef = useRef<HTMLInputElement>(null);
+  const birthRef = useRef<HTMLInputElement>(null);
+  const imageRef = useRef<HTMLInputElement>(null);
+
   const handleClick = (e : React.MouseEvent<HTMLDivElement>) => {
     e.preventDefault();
     setShowDetails((prev) => !prev);
@@ -15,6 +22,33 @@ const Card = ({puppy} : IPuppyComponentProps) => {
 
   const handleDelete = async() => {
     await fetch(`${API_LINK}/${puppy.id}`, {method:'DELETE', mode: 'cors', headers: {'Content-Type': 'application/json'}});
+    window.location.reload();
+  }
+
+  const handleEdit = () => {
+    setShowEditForm((prev) => !prev);
+  }
+
+  const handleSubmit = async(e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const breed = breedRef.current ? breedRef.current.value : '';
+    const name = nameRef.current ? nameRef.current.value : '';
+    const birthDate = birthRef.current ? birthRef.current.value : '';
+    const image = imageRef.current ? imageRef.current.value : '';
+
+    const body: CreatePuppy = {
+      breed: breed,
+      name: name,
+      birthDate: birthDate,
+      image: image
+    }
+    await fetch(`${API_LINK}/${puppy.id}`, {
+      method: 'PUT', 
+      mode: 'cors', 
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(body)
+    })
+  
     window.location.reload();
   }
 
@@ -32,9 +66,17 @@ const Card = ({puppy} : IPuppyComponentProps) => {
         }
       </div>
       <div className='btnContainer'>
-        <button className='delete' onClick={() => { handleDelete()}}>Delete</button>
-        <button className='edit' onClick={(e: React.MouseEvent<HTMLButtonElement>) => {e.preventDefault(); handleDelete()}}>Edit</button>
+        <button className='delete' onClick={() => { handleDelete();}}>Delete</button>
+        <button className='edit' onClick={(e: React.MouseEvent<HTMLButtonElement>) => {e.stopPropagation(); e.preventDefault(); handleEdit()}}>Edit</button>
       </div>
+      {showEditForm && (        
+          <form className='editForm' onSubmit={handleSubmit}>
+            <input type='text' placeholder={'breed'} ref={breedRef} defaultValue={puppy.breed} required />
+            <input type='text' placeholder={'name'} ref={nameRef} defaultValue={puppy.name} required />
+            <input type='text' placeholder={'birthdate (yyyy-mm-dd)'} ref={birthRef} defaultValue={puppy.birthDate} required />
+            <input type='text' placeholder={'image url (if empty, a random image will be added)'} ref={imageRef} defaultValue={puppy.image} />
+            <button type='submit'>Submit</button>
+          </form>)}
     </div>
   )
 }
